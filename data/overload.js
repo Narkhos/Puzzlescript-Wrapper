3c7781a2cf7771c0463398abdc308ce03596d7ab
Narkhos <xavierdirez@hotmail.com>
2018-03-04 14:35:42 +0100

    prevent the use of youtube stream

---
+++ data/overload.js
@@ -0,0 +1,13 @@
+var old_redraw = redraw;
+redraw = function() {
+	nativeClearInstances();
+	old_redraw();
+}
+
+consolePrint = console.log;
+
+// Activate verbose_logging for debugging
+verbose_logging = true;
+
+// Prevent using youtube stream
+canYoutube = false;
